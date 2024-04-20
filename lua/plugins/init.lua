4a65504b412e5b1ffc59fb6ecd51900a8b03346d
Floris <floris@xylex.ai>
2024-04-20 23:57:23 +0200

    initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,89 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  --
+ {
+ 	"williamboman/mason.nvim",
+ 	opts = {
+ 		ensure_installed = {
+ 			"lua-language-server", "stylua", "rust-analyzer",
+ 			"html-lsp", "css-lsp" , "prettier"
+ 		},
+ 	},
+ },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          debounce = 75
+          
+        
+        }
+      })
+      require("copilot.suggestion").is_visible()
+      require("copilot.suggestion").toggle_auto_trigger()
+    end,
+  },
+
+
+ {
+ 	"nvim-treesitter/nvim-treesitter",
+ 	opts = {
+ 		ensure_installed = {
+ 			"vim", "lua", "vimdoc",
+      "html", "css"
+ 		},
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        disabled = {"rust"},
+      },
+ 	},
+ },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1 
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  }
+
+
+}
